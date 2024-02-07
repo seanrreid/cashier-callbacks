0af6165865cecfe0b2ffaf0493c1802f0f8c43ac
Sean R Reid <sean@torchcodelab.com>
2024-02-06 21:47:08 -0500

    Updated console logs for example3

--- example3.js
+++ example3.js
@@ -6,7 +6,7 @@ function buyStuff(total, haveCash) {
     const payment = setTimeout(function () {
         if (haveCash === true) {
             const myChange = getChange(100, total);
-            console.log("my change is:", myChange)
+            console.log(`My change is $${myChange}.`);
             return myChange;
         }
     }, 100);
@@ -14,8 +14,9 @@ function buyStuff(total, haveCash) {
 }
 
 function getChange(payment, total) {
-  let change = payment - total
-  return change;
+    console.log(`The total is $${total}.\nI paid $${payment}`);
+    let change = payment - total;
+    return change;
 }
 
-console.log(buyStuff(10, true))
+buyStuff(10, true);
